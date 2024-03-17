5265cc63534fd5ee302b388a2037d2a5a46f2523
Adrienne Eror <40647605+ladyadi@users.noreply.github.com>
2024-03-17 10:12:38 -0600

    Updates

---
+++ precache-manifest.016405713a1b308d5462a202958cef50.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "c95efb098b45404ffd1c",
+    "url": "/static/css/main.d170dff3.chunk.css"
+  },
+  {
+    "revision": "c95efb098b45404ffd1c",
+    "url": "/static/js/main.c95efb09.chunk.js"
+  },
+  {
+    "revision": "53e043d836eaae109700",
+    "url": "/static/css/1.64c51182.chunk.css"
+  },
+  {
+    "revision": "53e043d836eaae109700",
+    "url": "/static/js/1.53e043d8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7035fd20987ccb003942378810f1093a",
+    "url": "/static/media/Beard_of_Bees_Logo.7035fd20.jpg"
+  },
+  {
+    "revision": "7a09f1f4c7c50676f87b1323e77a2f4c",
+    "url": "/static/media/michael-hardman.7a09f1f4.png"
+  },
+  {
+    "revision": "8746dc96311d0bfb8abfbc744624a720",
+    "url": "/static/media/aaron-hardman.8746dc96.png"
+  },
+  {
+    "revision": "5150c090114b674472edcc805081fa4e",
+    "url": "/static/media/ryan-hardman.5150c090.png"
+  },
+  {
+    "revision": "c2e21fe6f4ba9582c893582367faf785",
+    "url": "/static/media/chase-howell.c2e21fe6.png"
+  },
+  {
+    "revision": "1b2662894b11c09063c16597edaa0965",
+    "url": "/static/media/jesse-hardman.1b266289.png"
+  },
+  {
+    "revision": "e596541b9c29c51f544f3bcebe909ebc",
+    "url": "/static/media/run_from_my_heart.e596541b.png"
+  },
+  {
+    "revision": "7099addc64a88e7fc64c2db73ec4d725",
+    "url": "/static/media/the_grey.7099addc.png"
+  },
+  {
+    "revision": "a35005c63abd4b5e29df194ea3ede61a",
+    "url": "/static/media/golden_spike.a35005c6.png"
+  },
+  {
+    "revision": "2cd574af7d3d0fa4f69fc5b7e659dfe0",
+    "url": "/static/media/way_home.2cd574af.png"
+  },
+  {
+    "revision": "aa9358416469962fc0ec0ac179896ebc",
+    "url": "/static/media/food-truck-rally.aa935841.jpg"
+  },
+  {
+    "revision": "4440cd83cb0eeb3a60425ad698d57b12",
+    "url": "/static/media/beard_at_kilby_court.4440cd83.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "be51ca90b23cff28cc85a0bf25d3c0d9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/beard-of-bees/precache-manifest.485bc5abb523079d060b9e28fc581434.js"
+  "/precache-manifest.016405713a1b308d5462a202958cef50.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/beard-of-bees/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
